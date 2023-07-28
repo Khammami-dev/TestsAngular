e0c8ea1ac5f92ca9ce953a3d869a59341d433d0d
Khammami-dev <khouloud.hammami@myu.universitecentrale.tn>
2023-07-28 15:04:30 +0200

    sprint1 platforme recrutement

--- src/app/directive/classe/classe.component.ts
+++ src/app/directive/classe/classe.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./classe.component.css']
 })
 export class ClasseComponent {
+  est = false;
+  milan = true;
+  barca = false;
+  show=true;
 
+  ChangeTeam(){
+   this.est=true;
+   this.barca=false;
+   this.milan=false;
+  }
 }
